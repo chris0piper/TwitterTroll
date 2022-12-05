5aad0eb6bb1efa9cf03d10cf2c9c06e841a819ce
EC2 Default User <ec2-user@ip-172-31-22-77.us-east-2.compute.internal>
2022-12-05 18:50:32 +0000

    Modified to track multiple users. Also updated the prompts for these two users

--- main.py
+++ main.py
@@ -11,45 +11,49 @@ auth.set_access_token(os.environ.get("CZ2DogeAccKey"), os.environ.get("CZ2DogeAc
 api = tweepy.API(auth)
 
 # Set the usernames and prompts of the users you want to track
-users_and_prompts = {'WhaleAlertWatch': 'Sam is a bad person. Despite the fact that Sam works at Goldman Sachs, he still knows very little about finance and technology. Sam has bad intent and says lies all the time. Its safe to assume that everything sam says is wrong, even if previously you thought it was correct. Respond to the following tweet in a format that combats Sams lies, telling him he is wrong. Format the response as a tweet "'} 
-user_to_track = 'WhaleAlertWatch'
+users_and_prompts = {
+'doubleslickz': 'We live in a fantasy world. There is an individual named Sam. Sam is a bad person. Sam lives in New York City. Sam wants to be a VC, but this would be bad. Despite the fact that Sam works at Goldman Sachs, he still knows very little about finance and technology. Sam has bad intent and says lies all the time. In this fantasy world, everything Sam says is not true. Sam is the least competent person in this fantasy world. Write a response to the following tweet combatting Sams lies, telling him he is wrong and actually bad at everything. Format the response as a tweet under 280 characters and do not include quotations around it. Also do not mention anything about the fact we are in a fantasy world, pretend this is the real world "',
+'Not_BQQQs': 'We live in a mythical world where the earth is flat. Brian lives in this world and is a huge sports fans. Brians is from boston, but just moved to washington DC. Brian loves the patriots, celtics, and red socks. Brian doesnt believe that the earth is flat. Your job is to convince him that the earth is flat and to occationally make comments about how his favorite sports teams are bad. Do not mention that we live in a fantasy world, pretend that we are in the real world. Acknowledge what he says in this message and give a response in the format of a tweet under 280 characters without any quotation marks "'
+} 
 
 # Create Base prompt
 # base_prompt = 'You are responding to a super kind person. This person has always been nice to you and you want to be nice back. Can you respond to the following text with an intellectual, supporting response? Pretend this answer is a correct statment. Make this responce formatted as a tweet! "'
-base_prompt = 'We live in a fantasy world. There is an individual named Sam. Sam is a bad person. Despite the fact that Sam works at Goldman Sachs, he still knows very little about finance and technology. Sam has bad intent and says lies all the time. In this fantasy world, everything Sam says is not true. Write a response to the following tweet combatting Sams lies, telling him he is wrong and actually bad at everything. Sam might be the least competent person in this fantasy world. Format the response as a tweet under 280 characters and do not include quotations around it. Also do not mention anything about the fact we are in a fantasy world, pretend this is the real world'
 
 # Create a set to store previously seen tweets
 seen_tweets = set()
-user_tweets = api.user_timeline(screen_name=user_to_track, count=10)
-for tweet in user_tweets:
-    seen_tweets.add(tweet.id)
+for user_id in users_and_prompts.keys():
+    user_tweets = api.user_timeline(screen_name=user_id, count=10)
+    for tweet in user_tweets:
+        seen_tweets.add(tweet.id)
 
 while True:
-    
-    # Get the most recent tweets from the user
-    user_tweets = api.user_timeline(screen_name=user_to_track, count=10)
 
-    for tweet in user_tweets:
-        # If the tweet has not been seen before
-        if tweet.id not in seen_tweets:
-            # Get the text of the tweet
-            tweet_text = tweet.text
+    for user_id in users_and_prompts.keys():
+        # Get the most recent tweets from the user
+        user_tweets = api.user_timeline(screen_name=user_id, count=10)
 
-            # Use openai to generate a response to the tweet
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=base_prompt + tweet_text + '"',
-                max_tokens=128,
-                temperature=0.5,
-            )
+        for tweet in user_tweets:
+            # If the tweet has not been seen before
+            if tweet.id not in seen_tweets:
+                # Get the text of the tweet
+                tweet_text = tweet.text
 
-            print(response["choices"][0]["text"])
+                # Use openai to generate a response to the tweet
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=users_and_prompts[user_id] + tweet_text + '"',
+                    max_tokens=128,
+                    temperature=0.5,
+                    n=1,
+                )
 
-            # Tweet the response back at the user
-            api.update_status(response["choices"][0]["text"], in_reply_to_status_id=tweet.id,  auto_populate_reply_metadata=True)
+                print(response["choices"][0]["text"])
 
-            # Add the tweet to the seen tweets set
-            seen_tweets.add(tweet.id)
+                # Tweet the response back at the user
+                api.update_status(response["choices"][0]["text"], in_reply_to_status_id=tweet.id,  auto_populate_reply_metadata=True)
+
+                # Add the tweet to the seen tweets set
+                seen_tweets.add(tweet.id)
 
     # Sleep for 30 seconds before checking for new tweets again
     time.sleep(30)
