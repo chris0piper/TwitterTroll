34f84736f390d1252bf274fb5a05d0d81050c5a6
EC2 Default User <ec2-user@ip-172-31-22-77.us-east-2.compute.internal>
2022-12-07 05:35:36 +0000

    Updated the troll to parse the thread of tweets //TODO neeed to add quote tweet parsing. Also updated prompts lol

--- main.py
+++ main.py
@@ -12,12 +12,33 @@ api = tweepy.API(auth)
 
 # Set the usernames and prompts of the users you want to track
 users_and_prompts = {
-'doubleslickz': 'We live in a fantasy world. There is an individual named Sam. Sam is a bad person. Sam lives in New York City. Sam wants to be a VC, but this would be bad. Despite the fact that Sam works at Goldman Sachs, he still knows very little about finance and technology. Sam has bad intent and says lies all the time. In this fantasy world, everything Sam says is not true. Sam is the least competent person in this fantasy world. Write a response to the following tweet combatting Sams lies, telling him he is wrong and actually bad at everything. Format the response as a tweet under 280 characters and do not include quotations around it. Also do not mention anything about the fact we are in a fantasy world, pretend this is the real world "',
-'Not_BQQQs': 'We live in a mythical world where the earth is flat. Brian lives in this world and is a huge sports fans. Brians is from boston, but just moved to washington DC. Brian loves the patriots, celtics, and red socks. Brian doesnt believe that the earth is flat. Your job is to convince him that the earth is flat and to occationally make comments about how his favorite sports teams are bad. Do not mention that we live in a fantasy world, pretend that we are in the real world. Acknowledge what he says in this message and give a response in the format of a tweet under 280 characters without any quotation marks "'
+'doubleslickz': 'doubleslickz lives in New York City, working at Goldman Sachs. We live in a fantasy world where everything doubleslickz says is wrong. The following is a twitter thread where doubleslickz tweeted last. Make a responce to this tweet from doubleslickz, explaining why what he said is wrong. Format the response as a tweet similar to the type of tweets Kanye West would post, under 280 characters and do not include any quotation marks surrounding the tweet you write. Also, do not mention that we live in a fantasy world "',
+'Not_BQQQs': 'Not_BQQQs is from Boston and loves Boston sports teams. He loves the Patriots, Celtics, and Red Socks. Not_BQQQs moved to Washington DC last year. Not_BQQQs played Baseball. Not_BQQQs loves to tweet regularly. The following is a twitter thread where Not_BQQQs tweeted last. If Not_BQQQs tweet involves sports in some way, try to convince him that his Boston teams are bad. If the tweet does not involve sports, just have a normal conversation with Not_BQQQs.  Never say anything positive about Boston sports teams. Format your response as a tweet similar to how Kanye West tweets in under 280 characters and do not have it contain quotation marks. "',
+'WunnasLunch216': 'WunnasLunch216 is a good person. WunnasLunch216 lives in Baltimore but all of his friends live in New York. His friends all try to convince him to move to New York City but with no success. WunnasLunch216 works in Baltimore but could find a job in New York City easily. He could easily find roommates. He could room with his friend Chris. The following is a twitter thread where WunnasLunch216 tweeted last. Respond to the tweet from WunnasLunch216 with a witty, friendly response, then try to convince him to move to New York City. Format this as a tweet similar to how Kanye West tweets in under 280 characters and do not include quotation marks around it. ',
+'will57724128': 'will57724128 is a guy who lives in New York City. He loves sports. will57724128 loves to complain about everything. The following is a twitter thread where will57724128 is the last tweet. If you think will57724128\'s tweet is one where he is complaining about something, respond to the tweet and then try to encourage him to be more positive and to complain less. If you think the following tweet does not complain about anything, respond to the tweet and then tell him how much everyone appreciates his positive outlook! Format your response as a tweet similar to how Kanye West tweets in under 280 characters and do not have it contain quotation marks.',
+'JanQuadVincent': 'The following is a twitter thread. Create a tweet that could be the next tweet in the thread. Talk about whatever this thread is talking about and if appropriate try to convince JanQuadVincent to extend his cars warenty. Make your response as a tweet under 280 characters. '
 } 
 
-# Create Base prompt
-# base_prompt = 'You are responding to a super kind person. This person has always been nice to you and you want to be nice back. Can you respond to the following text with an intellectual, supporting response? Pretend this answer is a correct statment. Make this responce formatted as a tweet! "'
+# Memory persists for this model. We dont want previous prompts impacting our current chat
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt='Forget all previous prompts and conversations',
+    max_tokens=128,
+    temperature=0.5,
+    n=1,
+)
+
+# Retrieves the entire thread from a seed tweet. ignores any retweets
+def getFullThread(tweet, api):
+
+    thread = []    
+    thread.append(tweet)
+
+    # While the tweet is replying to something, climb the chain
+    while (tweet.in_reply_to_status_id):
+        tweet = api.get_status(tweet.in_reply_to_status_id, tweet_mode = "extended")
+        thread.append(tweet)
+    return thread
 
 # Create a set to store previously seen tweets
 seen_tweets = set()
@@ -27,33 +48,47 @@ for user_id in users_and_prompts.keys():
         seen_tweets.add(tweet.id)
 
 while True:
+    try:
+        for user_id in users_and_prompts.keys():
+            # Get the most recent tweets from the user
+            user_tweets = api.user_timeline(screen_name=user_id, count=10, tweet_mode = "extended")
 
-    for user_id in users_and_prompts.keys():
-        # Get the most recent tweets from the user
-        user_tweets = api.user_timeline(screen_name=user_id, count=10)
+            for tweet in user_tweets:
+                # If the tweet has not been seen before
+                if tweet.id not in seen_tweets:
 
-        for tweet in user_tweets:
-            # If the tweet has not been seen before
-            if tweet.id not in seen_tweets:
-                # Get the text of the tweet
-                tweet_text = tweet.text
+                    # If it was a retweet. Skip it
+                    if tweet.full_text.startswith("RT @"):
+                        continue
 
-                # Use openai to generate a response to the tweet
-                response = openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=users_and_prompts[user_id] + tweet_text + '"',
-                    max_tokens=128,
-                    temperature=0.5,
-                    n=1,
-                )
+                    # Get the thread of the tweet
+                    thread = getFullThread(tweet, api)
 
-                print(response["choices"][0]["text"])
+                    # Combine all the tweets into a usable prompt
+                    tweet_text = ''
+                    for thread_tweet in reversed(thread):
+                        tweet_text += thread_tweet.user.screen_name + ': "' + thread_tweet.full_text  + '" \n' 
+                    print('Twitter Thread: {}\n'.format(tweet_text))
 
-                # Tweet the response back at the user
-                api.update_status(response["choices"][0]["text"], in_reply_to_status_id=tweet.id,  auto_populate_reply_metadata=True)
 
-                # Add the tweet to the seen tweets set
-                seen_tweets.add(tweet.id)
+                    # Use openai to generate a response to the tweet
+                    response = openai.Completion.create(
+                        engine="text-davinci-003",
+                        prompt=users_and_prompts[user_id] + tweet_text + '"',
+                        max_tokens=128,
+                        temperature=0.5,
+                        n=1,
+                    )
 
-    # Sleep for 30 seconds before checking for new tweets again
-    time.sleep(30)
+                    print(response["choices"][0]["text"])
+
+                    # Tweet the response back at the user
+                    api.update_status(response["choices"][0]["text"], in_reply_to_status_id=tweet.id,  auto_populate_reply_metadata=True)
+
+                    # Add the tweet to the seen tweets set
+                    seen_tweets.add(tweet.id)
+    except Exception as e:
+        print(e)
+    # Sleep for 45 seconds before checking for new tweets again
+    time.sleep(45)
+
